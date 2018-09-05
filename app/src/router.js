6efc14a1eda9551b0de566e32438d236419c688c
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-09-05 09:20:03 -0700

    adding home to the router

--- app/src/router.js
+++ app/src/router.js
@@ -1,8 +1,10 @@
 import VueRouter from 'vue-router';
 import Auth from './components/Auth';
+import Home from './components/Home';
 
 export default new VueRouter({
   routes: [
+    { path: '/', component: Home },
     { path: '/auth', component: Auth }
   ]
 });
