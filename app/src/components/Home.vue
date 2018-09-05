7aca1face63da3b9db725c09e505444cb0586655
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-09-05 13:42:33 -0700

    styling the goals

--- app/src/components/Home.vue
+++ app/src/components/Home.vue
@@ -1,11 +1,13 @@
 <template>
-  <div>
+  <div class="home">
     <h1 v-if="goals">Goals of all Users</h1>
     <ul>
       <li v-for="goal in goals"
         :key="goal.id"
       >
-        {{ goal.goal }}
+        <h2>{{ goal.user }}</h2>
+        <p>{{ goal.goal }}</p>
+        <h2 v-if="goal.complete">💯 ☑️ 🎉</h2>
       </li>
     </ul>
   </div>
@@ -31,5 +33,29 @@ export default {
 </script>
 
 <style scoped>
+.home {
+  margin-top: 30px;
+}
 
+h1 {
+  font-size: 2em;
+}
+
+h2 {
+  font-size: 1.75em;
+}
+
+p {
+    margin: 20px 0;
+}
+
+li {
+  list-style: none;
+  background: gray;
+  border-radius: 10px;
+  color: white;
+  padding: 20px;
+  margin: 20px auto;
+  width: 50vw;
+}
 </style>
--- app/src/main.js
+++ app/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './App.vue';
 import router from './router';
+import 'reset.css';
 
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
