45e4f3bba95d573481df588125b3f9e6c629fbe2
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-09-05 09:19:42 -0700

    writing fetches to login

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -3,6 +3,24 @@ const AUTH_URL = `${URL}/auth`;
 
 let token = '';
 
+function responseHandler(response) {
+  if(response.ok) return response.json();
+  return response.json().then(body => { 
+    throw body.error; 
+  });
+}
+
+function getHeaders() {
+  const headers = { 'Content-type': 'application/json' };
+  if(token) headers['Authorization'] = token;
+  return headers;
+}
+
+function storeUser(user) {
+  token = user.id;
+  window.localStorage.setItem('user', JSON.stringify(user));
+}
+
 export function checkForToken() {
   const json = window.localStorage.getItem('user');
   if(!json) return null;
@@ -10,4 +28,17 @@ export function checkForToken() {
   const user = JSON.parse(json);
   token = user.id;
   return user;
+}
+
+export function signIn(credentials) {
+  return fetch(`${AUTH_URL}/signin`, {
+    method: 'POST',
+    headers: getHeaders(),
+    body: JSON.stringify(credentials)
+  })
+    .then(responseHandler)
+    .then(user => {
+      storeUser(user);
+      return user;
+    });
 }
