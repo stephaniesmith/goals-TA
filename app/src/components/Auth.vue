7b273979d45b4e72dc08013d234ad2ec1e29c9b2
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-09-05 09:42:13 -0700

    toggle between signin and signup

--- app/src/components/Auth.vue
+++ app/src/components/Auth.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="auth">
+    <h2>{{label}}</h2>
+    <button @click="type = isSignUp ? 'singIn' : 'signUp'">
+      {{
+        isSignUp
+          ? "Already a user? Click here to sign in"
+          : "New? Click here to sign up"
+      }}
+    </button>
+    <pre>{{error}}</pre>
+
     <form @submit.prevent="handleSubmit">
       <input v-model="credentials.email" placehoder="email">
       <input v-model="credentials.password" placehoder="password">
@@ -9,7 +19,7 @@
 </template>
 
 <script>
-import { signIn } from '../services/api';
+import { signIn, signUp } from '../services/api';
 
 export default {
   name: 'Auth',
@@ -19,11 +29,22 @@ export default {
       credentials: {
         email: '',
         password: ''
-      }
+      },
+      type: 'SignIn',
+      error: null
     };
+  },
+  computed: {
+    isSignUp() {
+      return this.type === 'signUp';
+    },
+    label() {
+      return this.isSignUp ? 'Sign Up' : 'Sign In';
+    }
   },
   methods: {
     handleSubmit() {
+      this.error = null;
       signIn(this.credentials)
         .then(user => {
           this.onUser(user);
