80bf8c6042ea4345f9d26aaf6813548804e9471f
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-09-04 20:14:48 -0700

    create tables script

---
+++ server/scripts/create-tables.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const client = require('../db-client');
+
+client.query(`
+  CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(256) NOT NULL,
+    password VARCHAR(256) NOT NULL
+  );
+  CREATE TABLE IF NOT EXISTS goals (
+    id SERIAL PRIMARY KEY,
+    goal VARCHAR(256) NOT NULL,
+    complete BOOLEAN NOT NULL,
+    user_id INTEGER NOT NULL REFERENCES users(id)        
+  );
+`)
+  .then(
+    () => console.log('CREATED THEM TALBES!!!'),
+    err => console.error(err)
+  )
+  .then(() => client.end());
