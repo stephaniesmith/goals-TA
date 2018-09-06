0584bc84fe20c16a7d825c72e7d65aa4c35eb569
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-09-06 10:24:10 -0700

    added goals list

---
+++ app/src/components/GoalList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+  <h1>GOALSLIST</h1>
+    <ul>
+      <li v-for="goal in goals"
+        :key="goal.id"
+      >
+      {{goal.goal}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['goals']
+};
+</script>
+
+<style>
+
+</style>
--- app/src/components/Goals.vue
+++ app/src/components/Goals.vue
@@ -1,10 +1,42 @@
 <template>
-  <h1>GOALS!</h1>
+  <div>
+    <h1>GOALS!</h1>
+    <GoalList :goals="goals"/>
+    <AddGoal :onAdd="handleAdd"/>
+  </div>
 </template>
 
 <script>
-export default {
+import { getMyGoals, postGoal } from '../services/api';
+import GoalList from './GoalList';
+import AddGoal from './AddGoal';
 
+export default {
+  props: ['user'],
+  components: {
+    GoalList,
+    AddGoal
+  },
+  data() {
+    return {
+      goals: null
+    };
+  },
+  created() {
+    getMyGoals()
+      .then(goals => {
+        this.goals = goals;
+        console.log(this.goals);
+      });
+  },
+  methods: {
+    handleAdd(goal) {
+      postGoal(goal)
+        .then(goal => {
+          this.goals.push(goal);
+        });
+    }
+  }
 };
 </script>
 
