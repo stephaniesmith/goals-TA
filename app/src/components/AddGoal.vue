3d536f07ad91d661d0d36c64002c6f0ad129cc5e
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-09-06 10:39:50 -0700

    created add goal form

---
+++ app/src/components/AddGoal.vue
@@ -0,0 +1,27 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input>
+    <button>Submit</button>
+  </form>
+</template>
+
+<script>
+
+export default {
+  props: ['onAdd'],
+  data() {
+    return {
+      goal: ''
+    };
+  },
+  methods: {
+    handleSubmit() {
+      console.log('goal', this.goal);
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
